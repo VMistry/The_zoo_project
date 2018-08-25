e99b03975e4296d3fd3d7c3e10a53bb956577eb5
VMistry <virenmistry@sky.com>
2018-08-25 22:35:44 +0100

    Recreated interactivity

--- controllers/animal_controller.rb
+++ controllers/animal_controller.rb
@@ -17,4 +17,56 @@ class AnimalController < Sinatra::Base
     @zoo = Animals.new
     erb :'animals/new_animal'
   end
+
+
+  get "/:id" do
+    id = params[:id].to_i
+    @zoo = Animals.find(id)
+    erb :'animals/show'
+  end
+
+  get "/:id/edit" do
+    @title = "Edit"
+    #Gets the id on the selected page
+    id = params[:id].to_i
+    # Storing the object with that id in to @posts variable
+    @zoo = Animals.find(id)
+    erb :'animals/edit'
+  end
+
+  post "/" do
+    animals = Animals.new
+    animals.name = params[:name]
+    animals.enclosure_no = params[:enclosure_no]
+    animals.age = params[:age]
+    animals.species = params[:species]
+    animals.class = params[:class]
+    animals.animal_description = params[:animal_description]
+    animals.save
+    redirect "/"
+  end
+
+  put "/:id" do
+    id = params[:id].to_i
+    animal = Animals.find(id)
+    animal.name = params[:name]
+    animal.enclosure_no = params[:enclosure_no]
+    animal.age = params[:age]
+    animal.species = params[:species]
+    animal.class = params[:class]
+    animal.animal_description = params[:animal_description]
+    animal.save
+    redirect "/"
+  end
+
+  patch "/:id" do
+    "Update #{:id}"
+  end
+
+  delete "/:id" do
+    id = params[:id].to_i
+    Animals.destroy(id)
+    redirect '/'
+  end
+
 end
--- model/animals.rb
+++ model/animals.rb
@@ -6,6 +6,16 @@ class Animals
     connection = PG.connect(dbname: "animals")
   end
 
+  def save
+    conn = Animals.open_connection
+    if(!self.id)
+      sql = "INSERT INTO animals (name , enclosure_no , age , species , class , animal_description) VALUES ('#{self.name}', '#{self.enclosure_no.to_i}', '#{self.age.to_i}', '#{self.species}', '#{self.class}', '#{self.animal_description}')"
+    else
+      sql = "UPDATE animals SET name='#{self.name}', enclosure_no='#{self.enclosure_no.to_i}', age='#{self.age.to_i}', species='#{self.species}', class='#{self.class}', animal_description='#{self.animal_description}' WHERE id = #{self.id}"
+    end
+    conn.exec(sql)
+  end
+
   def self.all
     # Create an open_connection
     conn = self.open_connection
@@ -16,19 +26,32 @@ class Animals
     animals = results.map do |result|
       self.hydrate(result)
     end
-
     animals
   end
 
-  def self.hydrate(post_data)
+  # find on part of the data
+  def self.find(id)
+    conn = self.open_connection
+    sql = "SELECT * FROM animals WHERE id = #{id} LIMIT 1"
+    animal_result = conn.exec(sql)
+    animal = self.hydrate(animal_result[0])
+  end
+
+  def self.destroy(id)
+    conn = self.open_connection
+    sql = "DELETE FROM animals WHERE id = #{id}"
+    conn.exec(sql)
+  end
+
+  def self.hydrate(animal_data)
     animal = Animals.new
-    animal.id = post_data['id']
-    animal.name = post_data['name']
-    animal.enclosure_no = post_data['enclosure_no']
-    animal.age = post_data['age']
-    animal.species = post_data['species']
-    animal.class = post_data['class']
-    animal.animal_description = post_data['animal_description']
+    animal.id = animal_data['id']
+    animal.name = animal_data['name']
+    animal.enclosure_no = animal_data['enclosure_no'].to_i
+    animal.age = animal_data['age'].to_i
+    animal.species = animal_data['species']
+    animal.class = animal_data['class']
+    animal.animal_description = animal_data['animal_description']
 
     animal
   end
